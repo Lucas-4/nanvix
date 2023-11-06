3239fc92bba57ba13cac38bd0ccb2f21867af38a
unknown <diaslucas8822@gmail.com>
2023-11-06 15:39:51 -0300

    added semaphore functions to syscalls_table

--- src/kernel/sys/syscalls.c
+++ src/kernel/sys/syscalls.c
@@ -73,7 +73,7 @@ PUBLIC void (*syscalls_table[NR_SYSCALLS])(void)  = {
 	(void (*)(void))&sys_shutdown,
 	(void (*)(void))&sys_ps,
 	(void (*)(void))&sys_gticks,
-    NULL,
-    NULL,
-    NULL
+    (void (*)(void))&sys_semget,
+    (void (*)(void))&sys_semctl,
+    (void (*)(void))&sys_semop
 };
